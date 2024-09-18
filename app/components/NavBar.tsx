28f4cbe2ee3c63933864495ff30739637ed57a3a
Brian Waweru <brianwaweru627@gmail.com>
2024-09-19 01:33:27 +0300

    Updated navbar styling

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -3,9 +3,9 @@ import SearchInput from './SearchInput'
 
 const NavBar = () => {
   return (
-    <div id="navBar" className='m-3 flex flex-row justify-between'>
+    <div id="navBar" className='mb-6 flex flex-row justify-between'>
         <div>
-            <h1 id="appTitle" className='text-3xl'><strong>Kanisa Digital</strong></h1>
+            <h1 id="appTitle" className='text-5xl'><strong>Kanisa Digital</strong></h1>
             <p id='appSubtitle' className='text-sm'>The App for Christians. (John 15:4)</p>
         </div>
         <SearchInput />
