6fae746210be27aaf3501601f3cc87160187c317
Brian Waweru <brianwaweru627@gmail.com>
2024-09-19 01:33:48 +0300

    Changed button title to Play

--- app/components/PlayButton.tsx
+++ app/components/PlayButton.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const PlayButton = () => {
   return (
     <div>
-        <button className='btn btn-accent' onClick={() => console.log('add to cart')}>Add to Cart</button>
+        <button className='btn btn-accent' onClick={() => console.log('add to cart')}>Play</button>
     </div>
   )
 }
